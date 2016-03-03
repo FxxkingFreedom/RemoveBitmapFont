ea9ded2e833b9ce1989e10f02291b8cdaf191277
Tomokatsu Kumata <tkumata@gmail.com>
2016-03-04 06:38:23 +0900

    Fix bug.

--- removeBitmap.sh
+++ removeBitmap.sh
@@ -3,14 +3,11 @@ set -e
 
 # Change for your environment.
 DRPBXDIR="${HOME}/Dropbox/removeBitmap";
-tmpPrefix="tmp-ttf";
-tmpMa="tmp-msgMa.ttf";
-tmpPa="tmp-msgPa.ttf";
-tmpUIa="tmp-msgUIa.ttf";
 
 #
 # Main routine
 #
+tmpPrefix="rbf-tmp-ttf";
 BASEPATH="$(cd $(dirname $0) && pwd)";
 FONTNAME=$2;
 
@@ -24,9 +21,8 @@ fi
 
 cd /tmp/;
 
-if [ "$1" = "step1" ]
-then
-    if [ -f "${DRPBXDIR}/tmp-ttf0.ttf" ]; then
+if [ "$1" = "step1" ]; then
+    if [ -f "${DRPBXDIR}/${tmpPrefix}0.ttf" ]; then
         echo "";
         echo "Temporary file exists. Skip step.";
         echo "";
@@ -38,12 +34,12 @@ then
         # fontforge -script ${BASEPATH}/BreakeTTC.pe;
         python "${BASEPATH}"/pyFFctrl.py "$1" "${FONTNAME}" "$tmpPrefix";
         
-        for tmpTTF in $(ls . | grep "$tmpPrefix"); do
+        for tmpTTF in "$(ls . | grep $tmpPrefix)" ; do
             if [ -f "$tmpTTF" ]; then
                 # sh script for workaround fontforge bug. Thank you ricty team.
                 sh "$BASEPATH"/os2version_reviser.sh "$tmpTTF" &&
                 mv "$tmpTTF" "${DRPBXDIR}"/;
-                rm tmp-ttf*.bak;
+                rm ${tmpPrefix}*.ttf.bak;
             else
                 echo "$tmpTTF not found.";
             fi
@@ -54,22 +50,22 @@ then
         echo "";
         exit 0;
     fi
-elif [ "$1" = "step2" ]
-then
+elif [ "$1" = "step2" ]; then
     echo "";
-    echo "Please tmp-ttf0a.ttf, tmp-ttf1.ttf, tmp-ttf2.ttf and so on by ttfautohint.exe on Windows.";
+    echo "Please ${tmpPrefix}0a.ttf, ${tmpPrefix}1a.ttf, ${tmpPrefix}2a.ttf and so on by ttfautohint.exe on Windows.";
     echo "";
     exit 0;
-elif [ "$1" = "step3" ]
-then
-    if ls "${DRPBXDIR}" | grep "${tmpPrefix}[0-9]*a.ttf" ; then
+elif [ "$1" = "step3" ]; then
+    if ls "${DRPBXDIR}" | grep "${tmpPrefix}[0-9]a.ttf" ; then
         cp "${DRPBXDIR}"/"${tmpPrefix}"*a.ttf .;
         python "${BASEPATH}"/pyFFctrl.py "$1" "${FONTNAME}" "${tmpPrefix}"; # merge by python
         
         if [ -f "new_${FONTNAME}" ]; then
             mv "new_${FONTNAME}" "${DRPBXDIR}"/;
-            rm "${DRPBXDIR}"/"$tmpPrefix"*;
-            rm "$tmpPrefix"*;
+            
+            # remove temporary TTFs.
+            rm "${DRPBXDIR}"/"$tmpPrefix"*a.ttf;
+            rm "$tmpPrefix"*a.ttf;
             
             echo "";
             echo "End of step 3.";
@@ -77,7 +73,7 @@ then
             exit 0;
         else
             echo "";
-            echo "Merge TTC fail. Check pyFFctrl.py.";
+            echo "Merge TTC fail. Check ${BASEPATH}/pyFFctrl.py.";
             echo "";
             exit 1;
         fi
