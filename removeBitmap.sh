6fb0f65c751e39a50762b3362f455dbfd96593a7
tkumata <tkumata@gmail.com>
2016-03-04 18:48:50 +0900

    無駄排除

--- removeBitmap.sh
+++ removeBitmap.sh
@@ -12,10 +12,8 @@ BASEPATH="$(cd $(dirname $0) && pwd)";
 FONTNAME=$2;
 
 if [ $# -lt 2 ]; then
-    echo "";
     echo "Usage: bash /path/to/removeBitmap.sh step{1|2|3} fontfilename";
-    echo "ex) bash removeBitmap.sh step1 msgothic.ttc";
-    echo "";
+    echo "   ex) bash removeBitmap.sh step1 msgothic.ttc";
     exit 1;
 fi
 
@@ -24,9 +22,7 @@ cd /tmp/;
 if [ "$1" = "step1" ]; then
     # TODO: ちゃんとする
     if [ -f "${DRPBXDIR}/${tmpPrefix}0.ttf" ]; then
-        echo "";
         echo "Temporary file exists. Skip step.";
-        echo "";
         exit 0;
     fi
     
@@ -46,15 +42,11 @@ if [ "$1" = "step1" ]; then
             fi
         done
         
-        echo "";
         echo "End of step 1.";
-        echo "";
         exit 0;
     fi
 elif [ "$1" = "step2" ]; then
-    echo "";
     echo "Please ${tmpPrefix}0a.ttf, ${tmpPrefix}1a.ttf, ${tmpPrefix}2a.ttf and so on by ttfautohint.exe on Windows.";
-    echo "";
     exit 0;
 elif [ "$1" = "step3" ]; then
     if ls "${DRPBXDIR}" | grep "${tmpPrefix}[0-9]a.ttf" ; then
@@ -68,25 +60,17 @@ elif [ "$1" = "step3" ]; then
             rm "${DRPBXDIR}"/"$tmpPrefix"*a.ttf;
             rm "$tmpPrefix"*a.ttf;
             
-            echo "";
             echo "End of step 3.";
-            echo "";
             exit 0;
         else
-            echo "";
             echo "Merge TTC fail. Check ${BASEPATH}/pyFFctrl.py.";
-            echo "";
             exit 1;
         fi
     else
-        echo "";
         echo "*a.ttf not found.";
-        echo "";
         exit 0;
     fi
 else
-    echo "";
     echo "Invalid argv.";
-    echo "";
     exit 1;
 fi
