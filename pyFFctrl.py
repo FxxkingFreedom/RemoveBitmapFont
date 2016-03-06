d2b055d2ab48cc56ab50a6276b6f7b201bad0c0e
tkumata <tkumata@gmail.com>
2016-03-06 20:28:16 +0900

    python de gasp reset

--- pyFFctrl.py
+++ pyFFctrl.py
@@ -5,6 +5,13 @@ import sys
 import glob
 import fontforge
 
+def getprep(font):
+    if font.getTableData("prep") == None:
+        return "None"
+    prepAsm = font.getTableData("prep")
+    prepText = fontforge.unParseTTInstrs(prepAsm)
+    return prepText
+
 argvs = sys.argv
 argc = len(argvs)
 
@@ -16,6 +23,7 @@ if argc == 1:
 
 fontforge.setPrefs('CoverageFormatsAllowed', 1)
 
+# Step 1
 if argvs[1] == "step1":
     # Get TTC family names
     familyNames = fontforge.fontsInFile(argvs[2])
@@ -27,18 +35,29 @@ if argvs[1] == "step1":
     for familyName in familyNames:
         openName = "%s(%s)" % (argvs[2], familyName) # ex, "msgothic.ttc(MS UI Gothic)"
         tmpTTF = "%s%da.ttf" % (argvs[3], i)
-        print openName
+        
+        # Open font
         font = fontforge.open(openName)
+        print openName
+        
+        # Edit font
         font.encoding = 'UnicodeFull'
+        font.selection.all()
         font.simplify()
         font.round()
         font.autoHint()
         font.autoInstr()
+        font.gasp = ((65535, ('gridfit', 'antialias', 'symmetric-smoothing', 'gridfit+smoothing')),)
+        font.gasp_version = 1
+        
+        # Generate font
         font.generate(tmpTTF, flags = flags)
         font.close()
         i += 1
+
+    print "End breake TTC."
+# Step 3
 elif argvs[1] == "step3":
-    # Generate TTC.
     newTTCname = 'new_' + argvs[2]
     files = glob.glob(argvs[3] + '[0-9]a.ttf')
     fontX = []
@@ -48,13 +67,18 @@ elif argvs[1] == "step3":
         fontX.append(fontOpen)
         
     if len(fontX) > 1:
+        # Generate TTC.
         f = fontX[0]
         fontX.pop(0)
         f.generateTtc(newTTCname, (fontX), ttcflags = ("merge",), layer = 1)
     else:
-        pass
+        print "Fonts not found."
+        quit()
     
     for font in fontX:
         font.close()
+    f.close()
+    print "End generate TTC"
+# Other
 else:
     pass
