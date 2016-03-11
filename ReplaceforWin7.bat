7645bb4b32dbf0d63320b0b2ca54740019bb7ef6
Tomokatsu Kumata <tkumata@gmail.com>
2016-03-12 00:38:31 +0900

    add bat file

---
+++ ReplaceforWin7.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Effect is applyed after reboot.
+echo Please quit all software.
+echo ---
+pause
+
+if exist "C:\temp\FontsBackup\Original\msgothic.ttc" (
+    takeown /F %WINDIR%\Fonts\msgothic.ttc /A
+    icacls %WINDIR%\Fonts\msgothic.ttc /grant Administrators:F
+    move %WINDIR%\Fonts\msgothic.ttc C:\temp\tmpfileX
+    copy %HOMEPATH%\Dropbox\removeBitmap\new_msgothic.ttc C:\temp\tmpfileX
+    move C:\temp\tmpfileX %WINDIR%\Fonts\msgothic.ttc
+    echo ---
+    echo Finished re-install font.
+    echo ---
+    pause
+)
+
+echo ---
+echo Result
+dir %WINDIR%\Fonts\ms*
+
+echo ---
+pause
+REM reboot
