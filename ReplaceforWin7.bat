83d999391b4d6cb7406a1b2c0bc720e0d5f02c5e
Tomokatsu Kumata <tkumata@gmail.com>
2016-04-05 09:32:51 +0900

    new bat file.

--- ReplaceforWin7.bat
+++ ReplaceforWin7.bat
@@ -4,17 +4,44 @@ echo Please quit all software.
 echo ---
 pause
 
-if exist "C:\temp\FontsBackup\Original\msgothic.ttc" (
-    takeown /F %WINDIR%\Fonts\msgothic.ttc /A
-    icacls %WINDIR%\Fonts\msgothic.ttc /grant Administrators:F
-    move %WINDIR%\Fonts\msgothic.ttc C:\temp\tmpfileX1
-    copy %HOMEPATH%\Dropbox\removeBitmap\new_msgothic.ttc C:\temp\tmpfileX
-    move C:\temp\tmpfileX %WINDIR%\Fonts\msgothic.ttc
+set /p fontSelect="1:msgothic.ttc 2:meiryo.ttc? [1/2]"
+
+if "%fontSelect%"=="1" (
+    oldFont="msgothic.ttc"
+    newFont="new_msgothic.ttc"
+) else if "%fontSelect%"=="2" (
+    oldFont="meiryo.ttc"
+    newFont="new_meiryo.ttc"
+)
+
+if not exist "C:\temp\FontsBackup\Original" (
+    md "C:\temp\FontsBackup\Original"
+)
+
+if not exist "C:\temp\FontsBackup\Original\%oldFont%" (
+    takeown /F %WINDIR%\Fonts\%oldFont% /A
+    icacls %WINDIR%\Fonts\%oldFont% /grant Administrators:F
+    move %WINDIR%\Fonts\%oldFont% C:\temp\FontsBackup\Original
+    copy %HOMEPATH%\Dropbox\removeBitmap\%newFont% C:\temp\tmpfileX
+    move C:\temp\tmpfileX %WINDIR%\Fonts\%oldFont%
+    echo ---
+    echo Finished install.
+    echo ---
+    echo Result
+    dir %WINDIR%\Fonts\%oldFont%
+    pause
+    shutdown -r -t 0
+) else (
+    takeown /F %WINDIR%\Fonts\%oldFont% /A
+    icacls %WINDIR%\Fonts\%oldFont% /grant Administrators:F
+    move %WINDIR%\Fonts\%oldFont% C:\temp\tmpfileX1
+    copy %HOMEPATH%\Dropbox\removeBitmap\%newFont% C:\temp\tmpfileX
+    move C:\temp\tmpfileX %WINDIR%\Fonts\%oldFont%
     echo ---
     echo Finished re-install font.
     echo ---
     echo Result
-    dir %WINDIR%\Fonts\ms*
+    dir %WINDIR%\Fonts\%oldFont%
     pause
     shutdown -r -t 0
 )
